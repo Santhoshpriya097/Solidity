134233814fbf5a1ef30c280d4ceb2e413aed8701
Santhoshpriya097 <37013427+Santhoshpriya097@users.noreply.github.com>
2018-03-05 09:54:36 +0530

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.16;
 
- //import "./ownable1.sol";
- 
 contract Token{
     function totalSupply() constant returns (uint256 supply) {}
     function balanceOf(address _owner) constant returns (uint256 balance) {}
